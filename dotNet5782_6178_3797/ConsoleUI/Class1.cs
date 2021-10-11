7f59ae6d4409583bbf9c421d5aec88a7751b315c
ElliotYarmish <eliyarmish@gmail.com>
2021-10-11 21:48:53 +0300

    new welcome func

---
+++ dotNet5782_6178_3797/ConsoleUI/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome3797()
+        {
+            Console.WriteLine("I am also here!");
+        }
+
+    }
+}
