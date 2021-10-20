3b2388083d5af03afe6958575775e98ccf3d5744
msharoni <92259206+msharoni@users.noreply.github.com>
2021-10-20 23:37:10 +0300

    new structs

--- DAL/Class1.cs
+++ DAL/Class1.cs
@@ -4,20 +4,50 @@ namespace IDAL
 {
     namespace DO
     {
-        public struct BaseStation{
+        public struct Drone{
+            int Id;
+            string Model;
+            WeightCategories MaxWeight;
+            DroneStatuses Status;
+            double Battery;
 
         }
-        public struct{ }
-
-
-
-        class DataSource {
-            internal static Drone Drones[10];
-            internal static Station Stations[5];
-            internal static Customer Customers[100];
-            internal static Parcel Parcels[1000];
+        public struct Station {
+            int Id;
+            int Name;
+            double Longitude;
+            double Lattitude;
+            int ChargeSlots;
+         }
+        public struct DroneCharge {
+             int DroneId;
+             int StationId;
+         }
+        public struct Parcel {
+            int Id;
+            int Senderid;
+            int Targetid;
+            WeightCategories Weight;
+            Priorities Priority;
+            datetime Requested;
+            int DroneId;
+            datetime Scheduled;
+            datetime PickedUp;
+            datetime Delivered;
+        }
+        public struct Customer{
+            int Id;
+            string Name;
+            string Phone;
+            double Longitude;
+            double Lattitude;
         }
 
+
+        class RANDOM
+        { 
+
+        };
 
     }
 }
