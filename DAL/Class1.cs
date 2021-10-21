10605d5212ed5250c1780b3e39f575124e6264eb
msharoni <92259206+msharoni@users.noreply.github.com>
2021-10-21 18:08:12 +0300

    INtialize func

--- DAL/Class1.cs
+++ DAL/Class1.cs
@@ -4,45 +4,60 @@ namespace IDAL
 {
     namespace DO
     {
+        
         public struct Drone{
             int Id {get; set};
-            string Model;
-            WeightCategories MaxWeight;
-            DroneStatuses Status;
-            double Battery;
+            string Model {get; set};
+            WeightCategories MaxWeight {get; set};
+            DroneStatuses Status {get; set};
+            double Battery {get; set};
 
         }
         public struct Station {
-            int Id;
-            int Name;
-            double Longitude;
-            double Lattitude;
-            int ChargeSlots;
+            int Id {get; set};
+            int Name {get; set};
+            double Longitude {get; set};
+            double Lattitude {get; set};
+            int ChargeSlots {get; set};
          }
         public struct DroneCharge {
-             int DroneId;
-             int StationId;
+             int DroneId {get; set};
+             int StationId {get; set};
          }
         public struct Parcel {
-            int Id;
-            int Senderid;
-            int Targetid;
-            WeightCategories Weight;
-            Priorities Priority;
-            datetime Requested;
-            int DroneId;
-            datetime Scheduled;
-            datetime PickedUp;
-            datetime Delivered;
+            int Id {get; set};
+            int Senderid {get; set};
+            int Targetid {get; set};
+            WeightCategories Weight {get; set};
+            Priorities Priority {get; set};
+            DateTime Requested {get; set};
+            int DroneId {get; set};
+            DateTime Scheduled {get; set};
+            DateTime PickedUp {get; set};
+            DateTime Delivered {get; set};
         }
         public struct Customer{
-            int Id;
-            string Name;
-            string Phone;
-            double Longitude;
-            double Lattitude;
+            int Id {get; set};
+            string Name {get; set};
+            string Phone {get; set};
+            double Longitude {get; set};
+            double Lattitude {get; set};
         }
-
+        enum WeightCategories{
+            Light,
+            Medium,
+            Heavy
+        }
+        enum DroneStatuses{
+            Available,
+            maintenance,
+            Delivery
+        }
+        enum Priorities{
+            Regular,
+            Fast,
+            Emergency
+        }
 
         
 
--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -1,12 +1,31 @@
 namespace DalObject{
 
-    class DataSource
-        { 
+    class DataSource{ 
             internal static Drone[] Drones;
             internal static Station[] Stations;
             internal static Customer[] Customers;
             internal static Parcel[] Parcels;
-        };
+
+            internal class Config{
+                static void Intalize(){
+                    Random r = new Random();
+                    int[] ids = {r.Next(9999999,100000000),r.Next(9999999,100000000),r.Next(9999999,100000000)};
+                    string[] Names = { "Rufus", "Bear", "Dakota", "Fido","Vanya", "Samuel", "Koani", "Volodya", "Prince", "Yiska" };
+                    int IndexStation = r.Next(Names.Length());
+                    int IndexCustomer1 = r.Next(Names.Length());
+                    int IndexCustomer2 = r.Next(Names.Length());
+                    int WeightLevel = r.Next(3);
+                    Drone D = new Drone { Id =ids[0], Model= "", MaxWeight = (WeightCategories)WeightLevel  , Status= (DroneStatuses)r.Next(3), Battery = r.Next(101)};
+                    Station S = new Station {Id = r.Next(9999999,100000000), Name= Names[IndexStation], Longitude= , Lattitude= , ChargeSlots = };
+                    Customer C1 = new Customer {Id = ids[1]), Name= Names[IndexCustomer1], Phone= r.Next(999999999,10000000000), Longitude = , Lattitude=  };
+                    Customer C2 = new Customer {Id = ids[2]), Name= Names[IndexCustomer2], Phone= r.Next(999999999,10000000000), Longitude = , Lattitude=  };
+                    Parcel P = new Parcel {Id = r.Next(9999999,100000000),Senderid = ids[1] , Targetid = ids[2],Weight = (WeightCategories)r.Next(WeightLevel+1),Priority = (Priorities)r.Next(3), Requested = DateTime.Now, DroneId = ids[0], Scheduled = new DateTime(), PickedUp= new DateTime(),Delivered = new DateTime() };
+                    
+
+                }
+
+            }
+        }
 
         
 }
