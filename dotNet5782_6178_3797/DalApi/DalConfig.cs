2cd47f3dfe1200e440c852e263634a2e7e8e5140
msharoni <msharoni@MSTS-VS2019B>
2022-01-13 21:52:09 +0200

    dalApi fixes

--- dotNet5782_6178_3797/DalApi/DalConfig.cs
+++ dotNet5782_6178_3797/DalApi/DalConfig.cs
@@ -7,15 +7,34 @@ namespace DalApi
 {
     class DalConfig
     {
+        //internal static string DalName;
+        //internal static Dictionary<string, string> DalPackages;
+        //static DalConfig()
+        //{
+        //    XElement dalConfig = XElement.Load(@"xml\dal-config.xml");
+        //    DalName = dalConfig.Element("dal").Value;
+        //    DalPackages = (from pkg in dalConfig.Element("dal-packages").Elements()
+        //                   select pkg
+        //                  ).ToDictionary(p => "" + p.Name, p => p.Value);
+        //}
         internal static string DalName;
         internal static Dictionary<string, string> DalPackages;
+        internal static Dictionary<string, string> DalNamespaces;
+        internal static Dictionary<string, string> DalClasses;
         static DalConfig()
         {
             XElement dalConfig = XElement.Load(@"xml\dal-config.xml");
             DalName = dalConfig.Element("dal").Value;
             DalPackages = (from pkg in dalConfig.Element("dal-packages").Elements()
                            select pkg
-                          ).ToDictionary(p => "" + p.Name, p => p.Value);
+                           ).ToDictionary(p => "" + p.Name, p => p.Value);
+            DalNamespaces = (from pkg in dalConfig.Element("dal-packages").Elements()
+                             select pkg
+                          ).ToDictionary(p => "" + p.Name, p => p.Attribute("namespace").Value);
+            DalClasses = (from pkg in dalConfig.Element("dal-packages").Elements()
+                          select pkg
+                          ).ToDictionary(p => "" + p.Name, p => p.Attribute("class").Value);
+
         }
     }
     public class DalConfigException : Exception
