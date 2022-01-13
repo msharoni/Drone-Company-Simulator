1245a284b847607a0767c7d79e3f316973987f6c
msharoni <msharoni@MSTS-VS2019B>
2022-01-14 00:55:57 +0200

    finish simulator

--- dotNet5782_6178_3797/BL/DroneSim.cs
+++ dotNet5782_6178_3797/BL/DroneSim.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using static System.Math;
 namespace BL
 {
     
@@ -46,8 +47,64 @@ namespace BL
                             }
                         }
                         break;
+                    case BO.DroneStatuses.Delivery:
+                        BO.Parcel parcel = bl.DisplayParcel(drone.Parcel.Id);
+                        if (parcel.PickedUp == null)
+                        {
+                            if (bl.Distance(drone.CurrentLocation, bl.DisplayCustomer(parcel.Sender.Id).Location) < DroneSpeed)
+                                bl.PickUpParcel(DroneId);
+                            else
+                            {
+                                BO.Location location = LocationCalculater(drone.CurrentLocation, bl.DisplayCustomer(parcel.Sender.Id).Location, DroneSpeed);
+                                bl.UpdateDroneLocation(DroneId, drone.Battery - (BatteryUsage[1] * DroneSpeed), location);
+                            }
+                        }
+                        else
+                        {
+                            if (bl.Distance(drone.CurrentLocation, bl.DisplayCustomer(parcel.Reciver.Id).Location) < DroneSpeed)
+                                bl.DeliverParcel(DroneId);
+                            else
+                            {
+                                BO.Location location = LocationCalculater(drone.CurrentLocation, bl.DisplayCustomer(parcel.Reciver.Id).Location, DroneSpeed);
+                                int Weight = (int)drone.Parcel.Weight;
+                                bl.UpdateDroneLocation(DroneId, drone.Battery - (BatteryUsage[Weight+1] * DroneSpeed), location);
+                            }
+                        }
+
+                        break;
+                    
                 }
             }
         }
+        private BO.Location LocationCalculater(BO.Location from, BO.Location to, double Speed)
+        {
+            double radian = PI / 180;
+            double degree = 180 / PI;
+            double la1 = from.Lattitude * radian;
+            double lo1 = from.Longitude * radian;
+            double Ad = Speed / 6376.5;
+            BO.Location l = new BO.Location();
+            var teth = Bearing(from, to);
+            l.Lattitude = Asin(Sin(la1)* Cos(Ad) + Cos(la1)* Sin(Ad)* Cos(teth));
+            l.Longitude = lo1 + Atan2(Sin(teth)* Sin(Ad)* Cos(la1), Cos(Ad) - Sin(la1)* Sin(l.Lattitude));
+            l.Lattitude *= degree;
+            l.Longitude *= degree;
+            l.Longitude = (l.Longitude + 540) % 360 - 180;
+            return l;
+        }
+
+        private double Bearing(BO.Location from, BO.Location to)
+        {
+            double radian = PI / 180;
+            double la1 = from.Lattitude * radian;
+            double lo1 = from.Longitude * radian;
+            double la2 = to.Lattitude * radian;
+            double lo2 = to.Longitude * radian;
+            double deltaLo = lo2 - lo1;
+            double X = Cos(la2) * Sin(deltaLo);
+            double Y = Cos(la1) * Sin(la2) - Sin(la1) * Cos(la2) * Cos(deltaLo);
+            return Atan2(X, Y);
+        }
+
     }
 }
